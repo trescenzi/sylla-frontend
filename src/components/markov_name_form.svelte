08441308a5c83a7060e7ec64de96b73591ae12db
Thomas Crescenzi <trescenzi@gmail.com>
2020-08-08 15:31:39 -0400

    feat: add form for markov generation

--- src/components/markov_name_form.svelte
+++ src/components/markov_name_form.svelte
@@ -1,14 +1,16 @@
 <script>
-  import {generateNames} from '../helpers/sylla-api.js';
+  import {generateMarkovNames} from '../helpers/sylla-api.js';
   import NameTable from './name_table.svelte';
   let numNames = 10;
-  let numSyllablesPerName = 3;
+  let maxSyllablesPerName = 4;
+  let minSyllablesPerName = 2;
   let names = [];
 
   function handleSubmit(event) {
     event.preventDefault();
-    generateNames({
-      numSyllablesPerName,
+    generateMarkovNames({
+      maxSyllablesPerName,
+      minSyllablesPerName,
       numNames,
     }).then(ns => names = ns);
   }
@@ -17,7 +19,10 @@
 
 <form on:submit={handleSubmit}>
   <label>
-    Number of Syllables: <input type="number" name="numSyllables" bind:value={numSyllablesPerName} />
+    Min Syllables Per Name: <input type="number" name="numSyllables" bind:value={minSyllablesPerName} />
+  </label>
+  <label>
+    Max Syllables Per Name: <input type="number" name="numSyllables" bind:value={maxSyllablesPerName} />
   </label>
   <label>
     Number of Names: <input type="number" name="numNames" bind:value={numNames} />
--- src/helpers/sylla-api.js
+++ src/helpers/sylla-api.js
@@ -46,3 +46,27 @@ export function generateNames(input = {}) {
     console.error(e);
   }
 }
+
+export function generateMarkovNames(input = {}) {
+  try {
+    const body = JSON.stringify(input);
+
+    return fetch(`${API_URL}/markov/names`, {
+      method: 'POST',
+      mode: 'cors',
+      cache: 'no-cache',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body
+    })
+    .then(response => {
+      return response.json();
+    })
+    .then(({names}) => names)
+    .catch(e => console.error(e));
+  } catch(e) {
+    console.error('FAILED TO STRINGIFY BODY');
+    console.error(e);
+  }
+}
