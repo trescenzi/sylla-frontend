7108b5067614cb353b6f2e06a066dfbae8783918
Thomas Crescenzi <trescenzi@gmail.com>
2020-08-07 22:52:20 -0400

    feat: add animated welcome text

---
+++ src/components/letters/AnimatedWord.svelte
@@ -0,0 +1,88 @@
+<script>
+	import A from './a.svelte';
+	import B from './b.svelte';
+	import C from './c.svelte';
+	import D from './d.svelte';
+	import E from './e.svelte';
+	import F from './f.svelte';
+	import G from './g.svelte';
+	import H from './h.svelte';
+	import I from './i.svelte';
+	import J from './j.svelte';
+	import K from './k.svelte';
+	import L from './l.svelte';
+	import M from './m.svelte';
+	import N from './n.svelte';
+	import O from './o.svelte';
+	import P from './p.svelte';
+	import Q from './q.svelte';
+	import R from './r.svelte';
+	import S from './s.svelte';
+	import T from './t.svelte';
+	import U from './u.svelte';
+	import V from './v.svelte';
+	import W from './w.svelte';
+	import X from './x.svelte';
+	import Y from './y.svelte';
+	import Z from './z.svelte';
+  import { createEventDispatcher } from 'svelte';
+
+	const dispatch = createEventDispatcher();
+
+	const letters = {A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z};
+	
+	export let word = '';
+	let wordLetters = [];
+  function kickOffAnimation(word) {
+    wordLetters = word.toUpperCase().split('').reduce((acc, letter) => [
+      ...acc,
+      {
+        visible: false,
+        component: letters[letter],
+      },
+    ], []);
+
+    animateLoop();
+  }
+	function animateWord(word) {
+    if (wordLetters.length) {
+      wordLetters = [];
+      setTimeout(() => kickOffAnimation(word), 1750);
+    } else {
+      kickOffAnimation(word);
+    }
+	}
+	
+	function animateLoop(i = 0) {
+		if (i < wordLetters.length) {
+			wordLetters[i] = {
+				...wordLetters[i],
+				visible: !wordLetters[i].visbile,
+			}
+			setTimeout(() => animateLoop(i+1), 250)
+    } else if (i !== 0 && i === wordLetters.length) {
+      setTimeout(() => dispatch('end'), 2300);
+    }
+	}
+
+  $: animateWord(word);
+</script>
+
+<style>
+	.word {
+		height: 60px;
+    display: block;
+	}
+</style>
+
+<div class="word">
+	{#each wordLetters as letter}
+		{#if letter.visible}
+      <svelte:component 
+        width={40} 
+        height={40} 
+        this={letter.component}
+      />
+		{/if}
+	{/each}
+</div>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,40 +2,29 @@
   import {generateName} from '../helpers/sylla-api.js';
   import { fade } from 'svelte/transition';
   import EnglishNamesForm from '../components/generate_english_names_form.svelte';
+  import AnimatedWord from '../components/letters/AnimatedWord.svelte';
 
-  let showName = true;
   let name = 'Hero';
 
-  const fadeOut = () => setTimeout(() => {
-    showName = false;
-    getName();
-  }, 2500);
-  const getName = () => setTimeout(() => generateName({}).then(n => {
-    showName = true;
-    name = n;
-    fadeOut();
-  }), 2500);
-  if (typeof window !== 'undefined') {
-    fadeOut();
-  }
+  const getName = () => {
+    if (typeof window !== 'undefined') {
+      generateName({}).then(n => {
+        name = n;
+      })
+    }
+  };
 </script>
 
 <style>
-	h1 {
-		font-size: 2.8em;
-		font-weight: 700;
-		margin: 0 0 0.5em 0;
-    text-transform: capitalize;
+	.welcome {
     justify-self: start;
 	}
 </style>
 
-<h1>
-  Welcome 
-  {#if showName}
-    <span in:fade out:fade="{{duration: 2500}}">{name}</span>
-  {/if}
-</h1>
+<div class="welcome">
+  <AnimatedWord word="Welcome" />
+  <AnimatedWord on:end={getName} word={name} />
+</div>
 
 <section>
   <h2>Simple, English based names:</h2>
